8f9774d65ecd19ecc9c49a4b5e193f0011ef1a57
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-02-14 22:41:15 +0600

    mongodb database connected

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ dotenv.config();
 //import middleware
 import errorHandlerMidleware from "./middleware/error-handler.js";
 import notFoundMiddleware from "./middleware/not-found.js";
+import connectDB from "./db/connect.js";
 
 app.get("/", (req, res) => {
   res.send("Hello wrold");
@@ -17,6 +18,15 @@ app.use(errorHandlerMidleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
