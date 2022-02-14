ab56e6830d03ffb069726f2d9025d2f1bd12eaf2
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-02-14 21:59:07 +0600

    .env file and error handler

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+
+//import middleware
+import errorHandlerMidleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
 
 app.get("/", (req, res) => {
   res.send("Hello wrold");
 });
+
+// custom middleware
+app.use(notFoundMiddleware);
+app.use(errorHandlerMidleware);
 
 const port = process.env.PORT || 5000;
 
