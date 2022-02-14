1ccb9328712baddceed007127d5261e57902a355
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-02-14 21:38:53 +0600

    server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello wrold");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
