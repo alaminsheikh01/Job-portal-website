3208e2471138d761754c25547fd114b32bd455bf
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-02-14 15:03:23 +0600

    register page setup awesome way

---
+++ src/pages/Register.js
@@ -0,0 +1,45 @@
+import React,{useState} from 'react'
+import {Logo, FormRow} from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+
+const initialState ={
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+}
+
+const Register = () => {
+ const [values, setValues] = useState(initialState)
+ // global state ad useNavigate
+
+const handleChange =(e) =>{
+    console.log(e.target)
+}
+
+const onSubmit =(e) =>{
+    e.preventDefault()
+    console.log(e.target)
+}
+
+
+  return (
+    <Wrapper className='full-page'>
+        <form className='form' onSubmit={onSubmit}>
+        <Logo/>
+        <h3>Logo</h3>
+        {/* name input  */}
+        <FormRow type="text" name="name" value={values.name} handleChange={handleChange}/>
+        {/* email input  */}
+        <FormRow type="email" name="email" value={values.email} handleChange={handleChange}/>
+
+        {/* password input  */}
+        <FormRow type="password" name="password" value={values.password} handleChange={handleChange}/>
+
+        <button type='submit' className='btn btn-block'>Submit</button>
+        </form>
+    </Wrapper>
+  )
+}
+
+export default Register
