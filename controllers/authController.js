76054d523a0fe9e24ce22818a9b9df9bbccb0b24
alaminsheikh01 <alaminsheikh9746@gmail.com>
2022-02-22 20:42:28 +0600

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,34 @@
 import User from "../model/userModel.js";
 import { StatusCodes } from "http-status-codes";
 
-const register = async (req, res, next) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(StatusCodes.CREATED).json({ user });
-  } catch (error) {
-    next(error);
+class CustomAPIError extends Error {
+  constructor(message) {
+    super(message);
   }
+}
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCodes = StatusCodes.BAD_REQUEST;
+  }
+}
+
+class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCodes = StatusCodes.NOT_FOUND;
+  }
+}
+
+const register = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new BadRequestError("Please provide all valuse");
+  }
+
+  const user = await User.create({ name, email, password });
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const login = (req, res) => {
--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,10 @@
 import { StatusCodes } from "http-status-codes";
 
 const errorHandlerMidleware = (err, req, res, next) => {
-  console.log(err);
+  console.log(err.message);
   const defaultError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: "Something went wrong, try again later",
+    msg: err.message || "Something went wrong, try again later",
   };
   if (err.name === "ValidationError") {
     defaultError.statusCode = StatusCodes.BAD_REQUEST;
@@ -13,8 +13,12 @@ const errorHandlerMidleware = (err, req, res, next) => {
       .map((item) => item.message)
       .join(".");
   }
-  //res.status(defaultError.statusCode).json({ msg: err });
-  res.status(defaultError.statusCode).json({ msg: defaultError.msg });
+  if (err.code && err.code === 11000) {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.msg = `${Object.keys(err.keyValue)}field has to unique`;
+  }
+  res.status(defaultError.statusCode).json({ msg: err });
+  // res.status(defaultError.statusCode).json({ msg: defaultError.msg });
 };
 
 export default errorHandlerMidleware;
